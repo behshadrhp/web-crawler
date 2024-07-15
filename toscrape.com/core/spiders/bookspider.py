4e9e330e0bc4bc24c7d40cab93bf19cf727a2c46
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-07-16 00:23:34 +0330

    create parse_book_page + crawl all information page book + save data json, csv

--- toscrape.com/core/spiders/bookspider.py
+++ toscrape.com/core/spiders/bookspider.py
@@ -11,12 +11,19 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
 
         for book in books:
-            yield{
-                "title": book.css("h3 a::text").get(),
-                "price": book.css(".product_price .price_color::text").get(),
-                "url": book.css("h3 a").attrib["href"]
-            }
+            # get more information about element
+            relative_url = book.css("h3 a::attr(href)").get()
         
+            # go to information page
+            if relative_url is not None:
+                if "catalogue/" in relative_url:
+                    book_url = "http://books.toscrape.com/" + relative_url
+                else:
+                    book_url = "http://books.toscrape.com/catalogue/" + relative_url
+                yield response.follow(book_url, callback=self.parse_book_page)
+        
+        
+        # next page
         next_page = response.css("li.next a::attr(href)").get()
         
         if next_page is not None:
@@ -26,3 +33,26 @@ class BookspiderSpider(scrapy.Spider):
                 next_page_url = "http://books.toscrape.com/catalogue/" + next_page
             yield response.follow(next_page_url, callback=self.parse)
         
+    def parse_book_page(self, response):
+        
+        # get data from ...
+        book = response.css(".product_page")
+        table_row = response.css("table tr")
+        
+        # clone data
+        yield{
+            "url": response.url,
+            "image": book.css("div.item img").attrib["src"],
+            "title": book.css("div.col-sm-6 h1::text").get(),
+            'category': book.xpath("//li[3]/a/text()").get(),
+            'stars': book.css("div.col-sm-6 p.star-rating ").attrib["class"],
+            'price': book.xpath("//p[@class='price_color']//text()").get(),
+            'upc': table_row[0].css("td::text").get(),
+            'product_type': table_row[1].css("td::text").get(),
+            'price_excl_tax': table_row[2].css("td::text").get(),
+            'price_incl_tax': table_row[3].css("td::text").get(),
+            'tax': table_row[4].css("td::text").get(),
+            'availability': table_row[5].css("td::text").get(),
+            'num_reviews': table_row[6].css("td::text").get(),
+            'description': book.xpath("//div[@id='product_description']/following-sibling::p/text()").get(),
+        }
