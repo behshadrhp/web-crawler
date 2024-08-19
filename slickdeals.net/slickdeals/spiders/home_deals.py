b01a792b15627de30569ccd4dcc66527301d1e00
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-19 10:55:18 +0330

    :memo: added next page + clim all data

--- slickdeals.net/slickdeals/spiders/home_deals.py
+++ slickdeals.net/slickdeals/spiders/home_deals.py
@@ -3,6 +3,8 @@ from scrapy.selector import Selector
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EXC
 
 class HomeDealsSpider(scrapy.Spider):
     name = "home_deals"
@@ -21,25 +23,35 @@ class HomeDealsSpider(scrapy.Spider):
     def parse(self, response):
         self.driver.get(response.url)
         
-        response_page = Selector(text=self.driver.page_source)
+        while True:
+            response_page = Selector(text=self.driver.page_source)
+            carts = response_page.xpath("//ul[@class='bp-p-filterGrid_items']/li/div[@class='bp-c-card_content']")
 
-        carts = response_page.xpath("//ul[@class='bp-p-filterGrid_items']/li/div[@class='bp-c-card_content']")
+            for cart in carts:
+                title = cart.xpath("./a/text()").get()
+                price = cart.xpath("./span[@class='bp-p-dealCard_price']/text()").get()
+                subtitle = cart.xpath("./span[@class='bp-c-card_subtitle']/text()").get()
+                sheared_time = cart.xpath("normalize-space(./figure/figcaption/div/span/text())").get()
+                store_name = cart.xpath("normalize-space(./figure/figcaption/div/text()[2])").get()
 
-        for cart in carts:
-            title = cart.xpath("./a/text()").get()
-            price = cart.xpath("./span[@class='bp-p-dealCard_price']/text()").get()
-            subtitle = cart.xpath("./span[@class='bp-c-card_subtitle']/text()").get()
-            sheared_time = cart.xpath("normalize-space(./figure/figcaption/div/span/text())").get()
-            store_name = cart.xpath("normalize-space(./figure/figcaption/div/text()[2])").get()
+                store = store_name.replace("by ", "")
 
-            store = store_name.replace("by ", "")
-
-            yield {
-                "title": title,
-                "price": price,
-                "subtitle": subtitle,
-                "sheared_time": sheared_time,
-                "store": store
-            }
-
+                yield {
+                    "title": title,
+                    "price": price,
+                    "subtitle": subtitle,
+                    "sheared_time": sheared_time,
+                    "store": store
+                }
+            
+            # try to go next page
+            try:
+                next_page_btn = WebDriverWait(self.driver, 10).until(
+                    EXC.element_to_be_clickable((By.CLASS_NAME, "bp-c-pagination_right"))
+                )
+                next_page_btn.click()
+            except:
+                break
+        
+        # close webdriver
         self.driver.close()
