4d3b898be5ca756708e1269a027819c1a270870d
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-26 07:59:39 +0330

    :art: create: VideoTurerialSpider + save data

---
+++ downloadly.ir/downloadly/spiders/video_tuterials.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class VideoTuterialsSpider(scrapy.Spider):
+    name = "video_tuterials"
+    allowed_domains = ["downloadly.ir"]
+    start_urls = ["https://downloadly.ir/download/elearning/video-tutorials/"]
+
+    def parse(self, response):
+        
+        items = response.xpath("//div[@class='vc_column-inner']/section/div/div/div/div/div/div/div/article")
+
+        for item in items:
+
+            title = item.xpath("./div/div/div[2]/h2/a/text()").get()
+            info = item.xpath("./div/div/div[2]/div[2]/text()").get()
+            page_url = item.xpath("./div/div/div[2]/h2/a/@href").get()
+
+
+            yield {
+                "title": title,
+                "info": info,
+                "page url": page_url
+            }
