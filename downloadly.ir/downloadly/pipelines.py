4ddb1643bc48ef14e71099841b1010e661caf89c
Behshad RahmanPour <behshad.rahmanpour@gmail.com>
2024-08-26 09:11:13 +0330

    :memo: added: mongo database to crawer -> not work

--- downloadly.ir/downloadly/pipelines.py
+++ downloadly.ir/downloadly/pipelines.py
@@ -5,9 +5,21 @@
 
 
 # useful for handling different item types with a single interface
+import logging
 from itemadapter import ItemAdapter
 
+import pymongo
 
-class DownloadlyPipeline:
+class MongoDownloadlyPipeline:
+    collection_name = "best_course"
+     
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient("mongodb+srv://root:bPil138APsssAVs0SAH@kingdoncl.a7law.mongodb.net/?retryWrites=true&w=majority&appName=KingDonCl")
+        self.db = self.client["downloadly"]
+
+    def close_spicer(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(item)
         return item
